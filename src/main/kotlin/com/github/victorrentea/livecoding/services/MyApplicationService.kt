1bebd3670b0f83567dadb7553ddeb30f78172844
GitHub Action <action@github.com>
2021-11-24 00:54:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/victorrentea/livecoding/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/victorrentea/livecoding/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.victorrentea.livecoding.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.victorrentea.livecoding.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/victorrentea/livecoding/services/MyProjectService.kt
+++ src/main/kotlin/com/github/victorrentea/livecoding/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.victorrentea.livecoding.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.victorrentea.livecoding.MyBundle
 
 class MyProjectService(project: Project) {
 
