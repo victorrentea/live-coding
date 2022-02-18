57b0efbd9efef31adb066c21c4ad3da23e81846e
Victor Rentea <victorrentea@gmail.com>
2022-02-18 08:58:55 +0200

    - Chapter: + history + clear button. Persisted in settings.

--- src/main/kotlin/com/github/victorrentea/livecoding/ux/chapter/ChapterService.kt
+++ src/main/kotlin/com/github/victorrentea/livecoding/ux/chapter/ChapterService.kt
@@ -4,18 +4,17 @@ import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.util.xmlb.XmlSerializerUtil
-import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatter.ofPattern
 
-@State(name = "ChapterService",  storages = [Storage("chapter4.xml")])
+@State(name = "ChapterService", storages = [Storage("chapter4.xml")])
 class ChapterService : PersistentStateComponent<ChapterService.ChapterState> {
     private val state = ChapterState(null, mutableListOf())
 
     fun currentChapter() = state.currentChapter
 
     fun startChapter(chapter: Chapter?) {
-        if (state.currentChapter != null) {
-            state.pastChapters += state.currentChapter !!
+        if (chapter != null) {
+            state.pastChapters += chapter
         }
         state.currentChapter = chapter
     }
@@ -23,15 +22,20 @@ class ChapterService : PersistentStateComponent<ChapterService.ChapterState> {
     fun chapterHistory() =
         state.pastChapters
             .groupBy { it.startTime().format(ofPattern("EEE, MMM dd")) }
-            .map { dayEntry -> "On " + dayEntry.key+":\n" + dayEntry.value
-                .map { it.startTime().format(ofPattern("HH:mm")) + " " + it.title }
-                .joinToString("\n")}
+            .map { dayEntry ->
+                "On " + dayEntry.key + ":\n" + dayEntry.value
+                    .map { it.startTime().format(ofPattern("HH:mm")) + " " + it.title }
+                    .joinToString("\n")
+            }
             .joinToString("\n\n")
 
     override fun getState() = state
 
     override fun loadState(state: ChapterState) = XmlSerializerUtil.copyBean(state, this.state)
 
-    data class ChapterState(var currentChapter: Chapter? = null, val pastChapters :MutableList<Chapter> = mutableListOf())
+    data class ChapterState(
+        var currentChapter: Chapter? = null,
+        val pastChapters: MutableList<Chapter> = mutableListOf()
+    )
 }
 
